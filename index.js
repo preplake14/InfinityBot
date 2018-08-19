8b2c5a2c47a299b948e0c4349b1206d3be1beced
preplake14 <preplake14@gmail.com>
2018-08-19 22:57:02 +0200

    First

---
+++ index.js
@@ -0,0 +1,30 @@
+
+const Commando = require('discord.js-commando');
+const bot = new Commando.Client();
+
+bot.registry.registerGroup('simple', 'simple');
+bot.registry.registerDefaults();
+bot.registry.registerCommandsIn(__dirname + '/commands');
+
+const INVITE = process.env.INVITE;
+const TOKEN = process.env.TOKEN;
+
+bot.on('ready',function(){
+   console.log("ready");
+});
+
+bot.on('message', function(message){
+    if(message.content == INVITE)
+    {
+        message.delete(0); //Supposed to delete message
+        message.member.send("Valid code, you have been given the invited role.");
+        let invited = message.member.guild.roles.find("name", "invited");
+        message.member.addRole(invited);
+    }
+});
+
+
+
+
+
+bot.login(TOKEN);
